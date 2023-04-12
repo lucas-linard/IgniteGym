a2929b649c3d441bed564bfd06a69909d89bc2d7
LucasAlencar <lucas.linard@outlook.com>
2023-04-12 15:33:16 -0300

    refactor: path mapping

--- babel.config.js
+++ babel.config.js
@@ -3,21 +3,24 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-    ["module-resolver", {
-      root: ["./src"],
-      alias: {
-        "@dtos": "./src/dtos",
-        '@assets': './src/assets',
-        '@components': './src/components',
-        '@screens': './src/screens',
-        '@storage': './src/storage',
-        '@utils': './src/utils',
-        '@services': './src/services',
-        '@hooks': './src/hooks',
-        '@contexts': './src/contexts',
-        '@routes': './src/routes',
-      }
-    }]
-  ]
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dtos': './src/dtos',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes'
+          }
+        },
+      ],
+    ],
   };
 };
