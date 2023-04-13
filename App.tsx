4cd2924a9ec9ff8be5365217087ab0d2953994e3
LucasAlencar <lucas.linard@outlook.com>
2023-04-13 10:02:02 -0300

    feat: App.tsx navigation

--- App.tsx
+++ App.tsx
@@ -1,26 +1,19 @@
-import { Text, View } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { theme } from "./src/theme";
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { StatusBar } from "expo-status-bar";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 import { Loading } from "@components/Loading";
 
-import { SignIn } from "@screens/SignIn";
+import { Routes } from "./src/routes";
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-  });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={theme}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <SignIn/> : <Loading />}
+      {fontsLoaded ? <Routes/> : <Loading />}
     </NativeBaseProvider>
   );
 }
