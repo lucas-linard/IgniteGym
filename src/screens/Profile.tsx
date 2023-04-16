c8782296c6d2197ec92428159ab9e8af88621fe8
LucasAlencar <lucas.linard@outlook.com>
2023-04-16 13:28:18 -0300

    feat: Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,123 @@
-import { Center, Text } from "native-base";
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+  useToast,
+} from "native-base";
 
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+const PHOTO_SIZE = 33;
 
 export function Profile() {
-    return (
-        <Center flex={1}>
-        <Text>Profile</Text>
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://instagram.fssa20-1.fna.fbcdn.net/v/t51.2885-19/235455129_229264355750651_6579950562147653022_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fssa20-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=Hux_7mUMd-cAX8vP8eL&edm=ACWDqb8BAAAA&ccb=7-5&oh=00_AfC5W5AmNdi78XQjZqNNA1lvUASkjDzZDmu_QxqXjYx7_Q&oe=643C97AA&_nc_sid=1527a3"
+  );
+  const toast = useToast();
+
+  async function handlePickPhoto() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        if (photoInfo.exists && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({ title: "A imagem deve ter no máximo 5MB", placement: "top", bgColor: "red.500" });
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: userPhoto }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity onPress={handlePickPhoto}>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input bg="gray.600" placeholder="Nome" />
+
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
+
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+            fontFamily="heading"
+          >
+            Alterar senha
+          </Heading>
+
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={4} />
         </Center>
-    );
+      </ScrollView>
+    </VStack>
+  );
 }
