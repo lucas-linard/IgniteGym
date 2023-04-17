885284f63837f4444df2ca7198aabb1510643694
LucasAlencar <lucas.linard@outlook.com>
2023-04-17 19:11:18 -0300

    feat: api SignUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,12 @@
-import { VStack, Image, Center, Text, Heading, ScrollView } from "native-base";
+import {
+  VStack,
+  Image,
+  Center,
+  Text,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
 
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
@@ -6,35 +14,61 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { Controller, useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
 
 type FormData = {
   name: string;
   email: string;
   password: string;
   passwordConfirmation: string;
-}
+};
 
 const signUpSchema = yup.object({
-  name: yup.string().required('Informe o nome.'),
-  email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
-  password: yup.string().required('Informe a senha.').min(8, 'No mínimo 8 caracteres.'),
-  passwordConfirmation: yup.string().required('Confirme a senha').oneOf([yup.ref('password'), "" ], 'As senhas não conferem.')
+  name: yup.string().required("Informe o nome."),
+  email: yup.string().required("Informe o e-mail.").email("E-mail inválido."),
+  password: yup
+    .string()
+    .required("Informe a senha.")
+    .min(8, "No mínimo 8 caracteres."),
+  passwordConfirmation: yup
+    .string()
+    .required("Confirme a senha")
+    .oneOf([yup.ref("password"), ""], "As senhas não conferem."),
+});
 
-})
 export function SignUp() {
   const navigation = useNavigation();
-  const { control ,handleSubmit, formState: { errors} } = useForm<FormData>({
-    resolver: yupResolver(signUpSchema)
+  const toast = useToast();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(signUpSchema),
   });
 
   function handleGoBack() {
     navigation.goBack();
   }
 
-  function handleSignUp(form: FormData) {
-    console.log(form)
+  async function handleSignUp({ name, email, password }: FormData) {
+    try {
+      const response = await api.post("/users", { name, email, password });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const errorMessage = isAppError
+        ? error.message
+        : "Não foi possível criar a conta.\n Tente novamente mais tarde";
+
+      toast.show({
+        title: errorMessage,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
     //navigation.navigate("signIn");
   }
 
@@ -74,14 +108,13 @@ export function SignUp() {
           <Controller
             control={control}
             name="name"
-            
             render={({ field: { onChange, value } }) => (
-              <Input 
+              <Input
                 placeholder="Nome"
                 onChangeText={onChange}
                 value={value}
                 errorMessage={errors.name?.message}
-                />
+              />
             )}
           />
           <Controller
@@ -103,12 +136,12 @@ export function SignUp() {
             control={control}
             name="password"
             render={({ field: { onChange, value } }) => (
-              <Input 
-              placeholder="Senha"
-              secureTextEntry
-              onChangeText={onChange}
-              value={value}
-              errorMessage={errors.password?.message}
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
               />
             )}
           />
@@ -116,22 +149,22 @@ export function SignUp() {
             control={control}
             name="passwordConfirmation"
             render={({ field: { onChange, value } }) => (
-              <Input 
-              placeholder="Confirme a senha"
-              secureTextEntry
-              onChangeText={onChange}
-              value={value}
-              errorMessage={errors.passwordConfirmation?.message}
-              onSubmitEditing={handleSubmit(handleSignUp)}
-              returnKeyType="send"
+              <Input
+                placeholder="Confirme a senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.passwordConfirmation?.message}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
               />
             )}
           />
 
-          
-          
-
-          <Button title="Criar e acessar" onPress={handleSubmit(handleSignUp)} />
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
         </Center>
 
         <Button
