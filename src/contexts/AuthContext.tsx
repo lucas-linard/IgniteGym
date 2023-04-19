9e5a65d5cd40cc0ee50108614dd85e4832f8937c
LucasAlencar <lucas.linard@outlook.com>
2023-04-19 11:56:23 -0300

    feat: Loading when fetching for userData

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,14 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+
+import { storageUserSave, storageUserGet } from "@storage/storageUser";
 
 import { api } from "@services/api";
-
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStorageData: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -19,6 +21,8 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
+    useState(true);
 
   async function signIn(email: string, password: string) {
     try {
@@ -26,13 +30,32 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user);
+        storageUserSave(data.user);
       }
     } catch (error) {
-        throw error
+      throw error;
     }
   }
+
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet();
+      if (userLogged) {
+        setUser(userLogged);
+        setIsLoadingUserStorageData(false);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ user, signIn, isLoadingUserStorageData }}>
       {children}
     </AuthContext.Provider>
   );
