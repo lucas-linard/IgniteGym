3a6d636fa169d350a852e040fe661f6787cd28b6
LucasAlencar <lucas.linard@outlook.com>
2023-04-19 11:24:08 -0300

    refactor: route context with useAuth hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,13 @@
-import { useContext } from "react";
 import { useTheme, Box } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-import { AuthContext } from "@contexts/AuthContext";
-
 import { AuthRoutes } from "./auth.routes";
+import { useAuth } from "@hooks/useAuth";
 
 export function Routes() {
   const { colors } = useTheme();
   
-  const contextData = useContext(AuthContext);
+  const { user } = useAuth();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
