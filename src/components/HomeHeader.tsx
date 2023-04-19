1754fb2bd502f06a9d5377e3f670e63a06ef8412
LucasAlencar <lucas.linard@outlook.com>
2023-04-19 12:06:37 -0300

    refactor: Homeheader uses user data from context

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,15 +2,15 @@ import { HStack, VStack, Text, Heading, Icon } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import { UserPhoto } from "./UserPhoto";
 import { TouchableOpacity } from "react-native";
-
+import { useAuth } from "@hooks/useAuth";
+import userPhotoDefaultImg from "@assets/userPhotoDefault.png";
 export function HomeHeader() {
+  const { user } = useAuth();
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
         alt="Foto do usuário"
-        source={{
-          uri: "https://instagram.fssa20-1.fna.fbcdn.net/v/t51.2885-19/235455129_229264355750651_6579950562147653022_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fssa20-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=Hux_7mUMd-cAX8vP8eL&edm=ACWDqb8BAAAA&ccb=7-5&oh=00_AfC5W5AmNdi78XQjZqNNA1lvUASkjDzZDmu_QxqXjYx7_Q&oe=643C97AA&_nc_sid=1527a3",
-        }}
+        source={ user.avatar ? {uri: user.avatar} : userPhotoDefaultImg }
         size={16}
         mr={4}
       />
@@ -19,7 +19,7 @@ export function HomeHeader() {
           Olá
         </Text>
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Lucas
+          {user.name}
         </Heading>
       </VStack>
       <TouchableOpacity>
