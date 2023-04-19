16c6930ebe172d7cbc77aa1af39dcc2ef41dbb76
LucasAlencar <lucas.linard@outlook.com>
2023-04-19 11:25:18 -0300

    refactor: App AuthContextProvider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { theme } from "./src/theme";
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import { Loading } from "@components/Loading";
-import { AuthContext } from "@contexts/AuthContext";
+import { AuthContextProvider } from "@contexts/AuthContext";
 import { Routes } from "./src/routes";
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -13,16 +16,9 @@ export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      <AuthContext.Provider value={{
-        user: {
-        id: '4',
-        name: "Lucas Alencar",
-        email: "lucasalencar@dev.com",
-        avatar: "Lucas.png"
-        }
-      }}>
-      {fontsLoaded ? <Routes/> : <Loading />}
-      </AuthContext.Provider>
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
