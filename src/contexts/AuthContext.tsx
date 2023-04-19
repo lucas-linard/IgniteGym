9ea6e774ea95c0e28bcbf4bc6e2ba034d1d35ddb
LucasAlencar <lucas.linard@outlook.com>
2023-04-19 11:24:59 -0300

    feat: AuthContext signIn function

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,39 @@
+import { createContext, useState } from "react";
+
+import { api } from "@services/api";
+
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext } from "react";
 
 export type AuthContextDataProps = {
-    user: UserDTO;
-}
+  user: UserDTO;
+  signIn: (email: string, password: string) => Promise<void>;
+};
 
-export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps)
+type AuthContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post("/sessions", { email, password });
+
+      if (data.user) {
+        setUser(data.user);
+      }
+    } catch (error) {
+        throw error
+    }
+  }
+  return (
+    <AuthContext.Provider value={{ user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,18 +1,55 @@
-import { VStack, Image, Center, Text, Heading, ScrollView } from "native-base";
+import { VStack, Image, Center, Text, Heading, ScrollView, useToast } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
 
 import { AuthNavigationRouteProps } from "@routes/auth.routes";
+import { useAuth } from "@hooks/useAuth";
 
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { AppError } from "@utils/AppError";
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const signInSchema = yup.object({
+  email: yup.string().required("Informe o e-mail."),
+  password: yup.string().required("Informe a senha."),
+});
 export function SignIn() {
+  const { signIn,user } = useAuth();
+  const toast = useToast()
+
   const navigation = useNavigation<AuthNavigationRouteProps>();
+  
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: yupResolver(signInSchema),
+  });
 
   function handleNewAccount() {
-    navigation.navigate("signUp");
+    navigation.navigate("signUp")
+  }
+
+  async function handleSignIn({email, password}: FormData) {
+    try {
+      await signIn(email, password)
+      
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const ErrorMessage = isAppError ? error.message : "Não foi possível fazer o login.\nTente novamente.";
+      toast.show({
+        title: ErrorMessage,
+        placement: "top",
+        bgColor: "red.500",
+      })
+    }
   }
   return (
     <ScrollView
@@ -47,15 +84,35 @@ export function SignIn() {
           >
             Acesse sua conta
           </Heading>
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={value}
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
-          <Input placeholder="Senha" secureTextEntry />
-
-          <Button title="Acessar" />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                value={value}
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+          <Button title="Acessar" onPress={handleSubmit(handleSignIn)}/>
         </Center>
         <Center mt={24}>
           <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
